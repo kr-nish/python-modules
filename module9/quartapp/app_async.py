7586e65d4f581bbfa06645278a7b7cc390e3ba79
labuser <labuser@ip-172-31-7-18.ap-south-1.compute.internal>
2025-10-14 17:34:33 +0000

    added quart app

---
+++ module9/quartapp/app_async.py
@@ -0,0 +1,22 @@
+import asyncio
+from quart import Quart, jsonify, request
+
+app = Quart(__name__)
+
+@app.route("/")
+async def home():
+    return jsonify({"Message":"Welcome home"})
+
+@app.route("/slow")
+async def slow_task():
+    await asyncio.sleep(3)
+    return jsonify({"Message":"This was the slow process"})
+
+@app.route("/parallel")
+async def parrallel_task():
+    async def task(id):
+        await asyncio.sleep(2)
+        return f"Task {id} done"
+    
+    results = await asyncio.gather(task(1), task(2), task(3))
+    return jsonify({"results": results})
