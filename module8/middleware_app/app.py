2cee0db349d681350f52821185fe7685363a15ff
labuser <labuser@ip-172-31-7-18.ap-south-1.compute.internal>
2025-10-14 17:09:27 +0000

    added day 9 content in same module8 folder

--- module8/middleware_app/app.py
+++ module8/middleware_app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+import time, cProfile, io, pstats
 
 app = Flask(__name__)
  
@@ -15,7 +16,7 @@ trace.set_tracer_provider(
 tracer = trace.get_tracer(__name__)
 
 # Export traces to Jaeger (via OTLP)
-otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:14317", insecure=True)
+otlp_exporter = OTLPSpanExporter(endpoint="http://jaeger:4317", insecure=True)
 trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
 
 # Also print traces to console (optional for debugging)
@@ -24,11 +25,27 @@ trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExp
 # Auto instrumentation
 FlaskInstrumentor().instrument_app(app)
 
+@app.before_request
+def start_profiler():
+    request.profiler = cProfile.Profile()
+    request.profiler.enable()
+
+@app.after_request
+def stop_profiler(response):
+    request.profiler.disable()
+    s = io.StringIO()
+    ps = pstats.Stats(request.profiler, stream=s).sort_stats("cumulative")
+    ps.print_stats(10)
+    print("\n__Profiling_REPORT----\n", s.getvalue())
+    return response
+    
+
+
 @app.route("/process", methods=["GET"])
 def process_data():
     with tracer.start_as_current_span("data_processing"):
-        import time; time.sleep(2)
+        import time; time.sleep(0.5)
         return jsonify({"Message": "this took longer than expected"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True) 
